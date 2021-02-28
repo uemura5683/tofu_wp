223467f87a837e0be58e0d3e41895f75273316cf
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-03-01 02:08:38 +0900

    テーマを編集・更新

---
+++ wp-content/themes/restaurant-and-cafe/sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The sidebar containing the main widget area.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Restaurant_and_Cafe
+ */
+
+if ( ! is_active_sidebar( 'right-sidebar' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area" role="complementary" itemscope itemtype="https://schema.org/WPSideBar">
+	<?php dynamic_sidebar( 'right-sidebar' ); ?>
+</aside><!-- #secondary -->
